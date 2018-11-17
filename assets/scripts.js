1aa0bf5e3ccccad7f0b655072c09d037272f57ba
Tonći Vatavuk <tonci@sistemi.hr>
2018-11-17 08:32:05 +0100

    WIP use TimelineJS v3.6.1

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,94 +1,97 @@
 ﻿
 $(function () {
 
-    // Process each Timeline DOM-element on the page
-    $(".timeline-embed").each(function() {
+  // Process each Timeline DOM-element on the page
+  $(".timeline-embed").each(function () {
 
-        // Define our timeline element, get moduleId
-        var timeLineElement = $(this);
+    // Define our timeline element, get moduleId
+    var timeLineElement = $(this);
 
-        // old sxc-initalizer, before 2sxc 8; included for reference if you have an older 2sxc
-        // var moduleId = timeLineElement.attr("data-moduleid");
-        // var sxc = $2sxc(moduleId);
+    // old sxc-initalizer, before 2sxc 8; included for reference if you have an older 2sxc
+    // var moduleId = timeLineElement.attr("data-moduleid");
+    // var sxc = $2sxc(moduleId);
 
-        // new method since 2sxc 8, using the simpler DOM initializer - see https://github.com/2sic/2sxc/wiki/JavaScript-%242sxc
-        var sxc = $2sxc(this);
+    // new method since 2sxc 8, using the simpler DOM initializer - see https://github.com/2sic/2sxc/wiki/JavaScript-%242sxc
+    var sxc = $2sxc(this);
 
-        // Load the data from the 2SexyContent module and define a callback
-        sxc.data.on("load", function (source, data) { 
-            processDataAndCreateTimeline(source, data, sxc.id, timeLineElement) 
-        }).load();
+    // Load the data from the 2SexyContent module and define a callback
+    sxc.data.on("load", function (source, data) {
+      processDataAndCreateTimeline(source, data, sxc.id, timeLineElement)
+    }).load();
 
-    });
+  });
 
-    // what should happen on loading of the data:
-    function processDataAndCreateTimeline (source, data, moduleId, timeLineElement) {
+  // what should happen on loading of the data:
+  function processDataAndCreateTimeline(source, data, moduleId, timeLineElement) {
 
-        // Re-format streams that are contained in data
-        var content = data.in.Default.List;
-        var listContent = data.in.ListContent.List[0];
-        var timelineData = getTimelineData(moduleId, content, listContent);
-
-        // Create the timeline
-        createStoryJS({
-            type: 'timeline',
-            width: '' + timeLineElement.width(),
-            height: timeLineElement.height(),
-            source: timelineData,
-            embed_id: timeLineElement.attr("id"),
-            start_at_slide: listContent.StartAtSlide,
-            start_zoom_adjust: listContent.StartZoomAdjust
-        });
-
+    // Re-format streams that are contained in data
+    var content = data.in.Default.List;
+    var listContent = data.in.ListContent.List[0];
+    var timelineData = getTimelineData(moduleId, content, listContent);
+    var additionalOptions = {
+      width: timeLineElement.width(),
+      height: timeLineElement.height(),
+      start_at_slide: listContent.StartAtSlide,
+      initial_zoom: listContent.StartZoomAdjust,
+      scale_factor: 1
     }
 
+    // Create the timeline
+    var timeline = new TL.Timeline(timeLineElement.attr("id"), timelineData, additionalOptions);
+  }
 
-    // Reformat data from 2sxcContent, so TimelineJS can work with it
-    function getTimelineData(moduleId, content, listContent) {
+  // Reformat data from 2sxcContent, so TimelineJS can work with it
+  function getTimelineData(moduleId, content, listContent) {
 
-        var isEditMode = $2sxc(moduleId).isEditMode();
+    var isEditMode = $2sxc(moduleId).isEditMode();
+
+    if (isEditMode) {
+      var listToolbar = $2sxc(moduleId).manage.getToolbar({ "entity": listContent, "action": "edit" });
+      listContent.Text += listToolbar;
+    }
+
+    var timelineData = {
+      "unique_id": "timeline-embed-" + moduleId,
+      "title": {
+        "startDate": FormatDate(listContent.StartDate),
+        "text": {
+          "headline": listContent.Headline,
+          "text": listContent.Text
+        }
+      },
+      "events": $.map(content, function (entry) {
 
         if (isEditMode) {
-            var listToolbar = $2sxc(moduleId).manage.getToolbar({ "entity": listContent, "action": "edit" });
-            listContent.Text += listToolbar;
+          var toolbar = $2sxc(moduleId).manage.getToolbar([{ "entity": entry, "action": "edit" }, { "entity": entry, "action": "new" }]);
+          entry.Body += toolbar;
         }
 
-        var timelineData = {
-            "timeline": {
-                "headline": listContent.Headline,
-                "type": "default",
-                "text": listContent.Text,
-                "startDate": FormatDate(listContent.StartDate),
-                "date": $.map(content, function (entry) {
-
-                    if (isEditMode) {
-                        var toolbar = $2sxc(moduleId).manage.getToolbar([{ "entity": entry, "action": "edit" }, { "entity": entry, "action": "new" }]);
-                        entry.Body += toolbar;
-                    }
-
-                    return {
-                        "startDate": FormatDate(entry.StartDate),
-                        "endDate": FormatDate(entry.EndDate),
-                        "headline": entry.Headline,
-                        "text": entry.Body,
-                        "classname": "sc-element",
-                        "asset": {
-                            "media": entry.Media,
-                            "credit": entry.MediaCredit,
-                            "caption": entry.MediaCaption
-                        }
-                    };
-                })
-            }
+        return {
+          "start_date": FormatDate(entry.StartDate),
+          "end_date": FormatDate(entry.EndDate),
+          "text": {
+            "headline": entry.Headline,
+            "text": entry.Body
+          },
+          "media": {
+            "url": entry.Media,
+            // "url": "http://free.pagepeeker.com/v2/thumbs.php?size=x&url=" + entry.Media,
+            "caption": entry.MediaCaption,
+            "credit": entry.MediaCredit
+          }
         };
-        return timelineData;
-    }
+      })
+    };
+    return timelineData;
+  }
 
-    function FormatDate(dateString) {
-        var date = new Date(dateString);
-        return date.getFullYear() + "," + (date.getMonth() + 1) + "," + date.getDate() + ",";
-    }
-
-
+  function FormatDate(dateString) {
+    var date = new Date(dateString);
+    return {
+      "year": date.getFullYear(),
+      "month": (date.getMonth() + 1),
+      "day": date.getDate()
+    };
+  }
 
 });
